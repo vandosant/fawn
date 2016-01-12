c572ea52d296e218d8830f3857d3ec8673bc8249
Scott Skender <sjskender@gmail.com>
2016-01-12 09:10:46 -0700

    Added test case for adding entities

--- src/reducers/cms.js
+++ src/reducers/cms.js
@@ -1,10 +1,41 @@
-export default function cmsAppState(state = 0, action) {
+function cmsAppState(state = [], action) {
   switch (action.type) {
     case 'INCREMENT':
       return state + 1;
     case 'DECREMENT':
       return state - 1;
+    case 'ADD_ENTITY':
+      return [];
     default:
       return state;
   }
 }
+
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
+
+const testAddEntity = () => {
+  const stateBefore = [];
+  const action = {
+    type: 'ADD_ENTITY',
+    id: 0,
+    text: 'Cms String'
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Cms String'
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    cmsAppState(stateBefore,action)
+  ).toEqual(stateAfter);
+  console.log('Passed!');
+};
+
+testAddEntity();
+export default cmsAppState;
